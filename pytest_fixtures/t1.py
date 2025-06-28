64531b5434cec31a5673eb6407250d13f3a2e8f1
Mohammad Shaheer Zaman <shaheer_zaman84@yahoo.com>
2025-06-28 18:56:27 +0530

    pytest fixtures

---
+++ pytest_fixtures/t1.py
@@ -0,0 +1,16 @@
+class C:
+    def f(self):
+        return 1
+
+    def g(self):
+        return 2
+
+
+def test_f():
+    c = C()
+    assert c.f() == 1
+
+
+def test_g():
+    c = C()
+    assert c.g() == 2
